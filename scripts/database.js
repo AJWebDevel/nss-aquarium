6a8631c49fa469271dc49772417d2ab05a128c5d
Abi Jones <abigaylej01@gmail.com>
2022-08-31 10:35:30 -0500

    Initial Commit

---
+++ scripts/database.js
@@ -0,0 +1,15 @@
+
+//this will be a database of all the fish displayed on the website
+
+const database = {
+    fish: [
+        {
+            id:
+                fishName: ,
+        species: ,
+        place of harvest: ,
+        length: ,
+        diet: ,
+        }
+    ]
+}
